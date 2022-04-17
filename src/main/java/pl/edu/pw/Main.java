c73d67346ed07ade74b66e29a75b36efdf70e692
lilartisto <orginalartis@gmail.com>
2022-04-17 22:55:15 +0200

    project initialization

---
+++ src/main/java/pl/edu/pw/Main.java
@@ -0,0 +1,13 @@
+package pl.edu.pw;
+
+import pl.edu.pw.geometry.Triangle;
+import pl.edu.pw.reading.MeshReader;
+
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+
+}
